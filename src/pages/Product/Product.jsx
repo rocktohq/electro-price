06e8c1241f3ee7f1546b876cc6b8c06363c456ee
Saidul Mursalin <itzmonir@gmail.com>
2023-09-21 00:33:35 +0600

    Product Card Updated

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -11,7 +11,7 @@ export default function Product({ product }) {
 
 
   return (
-    <div className="p-5 shadow-md rounded-md">
+    <div className="card p-5 shadow-md rounded-md">
       <figure className="h-48 w-full relative">
         <img className="h-full rounded-md w-full" src={thumbnail} alt={title} />
         <span className='bg-blue-500 text-white font-bold px-2 py-1 rounded-md absolute top-1 right-1'>{brand}</span>
@@ -22,7 +22,9 @@ export default function Product({ product }) {
           <p className="font-medium">Price: $<span className='text-xl'>{price}</span></p>
           <p className="font-medium text-end">Rating: <span className='bg-blue-300 rounded px-1 text-white'>{rating}</span></p>
         </div>
-        <button onClick={handleSeeDetails} className='btn btn-primary text-white rounded mt-5'>See Details</button>
+        <div className="card-actions mt-5">
+          <button onClick={handleSeeDetails} className='btn btn-primary text-white rounded w-full'>See Details</button>
+        </div>
       </div>
     </div>
   )
