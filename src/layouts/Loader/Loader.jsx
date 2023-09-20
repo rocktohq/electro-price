aed0a8728423c5d129cc30018c9e6764b6e5f730
Saidul Mursalin <itzmonir@gmail.com>
2023-09-21 00:33:00 +0600

    Loader Updated

--- src/layouts/Loader/Loader.jsx
+++ src/layouts/Loader/Loader.jsx
@@ -2,7 +2,7 @@
 export default function Loader() {
   return (
     <div className="text-center">
-      <span className="loading loading-bars text-primary loading-lg"></span>
+      <span className="loading loading-bars text-primary loading-lg h-screen"></span>
     </div>
   )
 }
